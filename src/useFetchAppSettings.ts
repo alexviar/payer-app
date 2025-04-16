1093c18f196ef7e53d3a25c552f5c9484936d19d
alexviar <alexanderviar@gmail.com>
2025-04-16 12:19:12 -0400

    feat: add app settings fetching

---
+++ src/useFetchAppSettings.ts
@@ -0,0 +1,40 @@
+import { useState, useEffect, useCallback } from 'react'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { Alert } from 'react-native'
+
+const CACHE_KEY = 'url'
+const REACT_APP_CONFIG_SERVER_URL = 'https://panel.internow.com.mx/api'
+
+export function useFetchAppSettings(appId: number) {
+  const [value, setValue] = useState<{ webUrl: string } | undefined>()
+
+  const fetchUrlFromServer = useCallback(async () => {
+    let value: { webUrl: string } | undefined
+    try {
+      const cachedUrl = await AsyncStorage.getItem(CACHE_KEY)
+      if (cachedUrl) {
+        value = { webUrl: cachedUrl }
+        setValue(value)
+      }
+    } catch { }
+
+    const endpoint = REACT_APP_CONFIG_SERVER_URL + '/setting/' + appId
+    try {
+      const response = await fetch(endpoint)
+      const { data } = await response.json()
+      AsyncStorage.setItem(CACHE_KEY, data.value)
+      setValue({ webUrl: data.value })
+    } catch (error) {
+      if (!value) {
+        Alert.alert('Error', 'No se pudo obtener la URL.')
+      }
+      console.log('Fetch Settings', endpoint, error)
+    }
+  }, [])
+
+  useEffect(() => {
+    fetchUrlFromServer()
+  }, [fetchUrlFromServer])
+
+  return { data: value }
+}
